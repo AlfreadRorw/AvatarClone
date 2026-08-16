d8c345bc29c3fb7f3ca54969a8f177a4ffeb09cf
AlfreadRorw <anggaselayar22@gmail.com>
2026-08-16 20:43:34 +0800

    update

--- Loader.lua
+++ Loader.lua
@@ -102,7 +102,7 @@ local AppList = {
     {path = "Applications/Server.lua", name = "Server"},
     {path = "Applications/Bundle.lua", name = "Bundle"},
     {path = "Applications/AvatarItems.lua", name = "AvatarItems"},
-    {path = "Applications/ServerJoiner.lua", name = "ServerJoiner"},
+    {path = "Applications/Server.lua", name = "Server"},
     {path = "Applications/WhoOnline.lua", name = "WhoOnline"},
     {path = "Applications/Messages.lua", name = "Messages"},
     {path = "Applications/Command.lua", name = "Command"},
